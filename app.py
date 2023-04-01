941505b711e9791dcd71fe0be2341a812a05b5ec
“Jiwoong <jiwoong0815@gmail.com>
2023-04-01 11:31:08 -0400

    thank you gpt

--- app.py
+++ app.py
@@ -5,51 +5,68 @@ def play_game(player_choice):
     computer_choice = random.choice(["rock", "paper", "scissors","spock","lizard"])
     if player_choice == computer_choice:
         result = "It's a tie!"
+        return result, computer_choice, "Draw"
     elif player_choice == "rock" and computer_choice == "scissors":
         result = "You win!"
+        return result, computer_choice, "Win"
     elif player_choice == "rock" and computer_choice == "lizard":
         result = "You win!"
+        return result, computer_choice, "Win"
     elif player_choice == "paper" and computer_choice == "rock":
         result = "You win!"
+        return result, computer_choice, "Win"
     elif player_choice == "paper" and computer_choice == "spock":
         result = "You win!"
+        return result, computer_choice, "Win"
     elif player_choice == "scissors" and computer_choice == "paper":
         result = "You win!"
+        return result, computer_choice, "Win"
     elif player_choice == "scissors" and computer_choice == "lizard":
         result = "You win!"
+        return result, computer_choice, "Win"
     elif player_choice == "spock" and computer_choice == "rock":
         result = "You win!"
+        return result, computer_choice, "Win"
     elif player_choice == "spock" and computer_choice == "scissors":
         result = "You win!"
+        return result, computer_choice, "Win"
     elif player_choice == "lizard" and computer_choice == "spock":
         result = "You win!"
+        return result, computer_choice, "Win"
     elif player_choice == "lizard" and computer_choice == "paper":
         result = "You win!"
+        return result, computer_choice, "Win"
     else:
         result = "You lose!"
-    return result, computer_choice
+        return result, computer_choice, "Lose"
 
-
+# Set up the UI
 st.title("Rock-Paper-Scissors Game")
-counter_win=0
-counter_lose=0
-counter_draw=0
+
 options = ["rock", "paper", "scissors","spock","lizard"]
 player_choice = st.radio("Choose your move:", options)
 
 if st.button("Play"):
-    result, computer_choice = play_game(player_choice)
+    result, computer_choice, outcome = play_game(player_choice)
     st.write(f"You chose {player_choice}, and the computer chose {computer_choice}.")
-    if result == "You win!":
+    if outcome == "Win":
         st.success(result, icon="🎉")
-        counter_win+=1
-    elif result == "You lose!":
+    elif outcome == "Lose":
         st.error(result, icon="👎")
-        counter_lose+=1
     else:
         st.warning(result, icon="🤔")
-        counter_draw+=1
 
-    
+    # add a counter
+    if "counter" not in st.session_state:
+        st.session_state["counter"] = {"Win": 0, "Lose": 0, "Draw": 0}
+
+    st.sidebar.header("Score")
+    st.session_state["counter"][outcome] += 1
+    st.sidebar.write(f"Win: {st.session_state['counter']['Win']}")
+    st.sidebar.write(f"Lose: {st.session_state['counter']['Lose']}")
+    st.sidebar.write(f"Draw: {st.session_state['counter']['Draw']}")
+        
 if st.button("Reset"):
+    st.session_state["counter"] = {"Win": 0, "Lose": 0, "Draw": 0}
     st.experimental_rerun()
+
