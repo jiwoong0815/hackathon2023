22ce00a6e1e2e790f8030b442290c2a04fce5523
“Jiwoong <jiwoong0815@gmail.com>
2023-04-01 10:46:07 -0400

    ok

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 import streamlit as st
 import random
 
-# Define the game logic
 def play_game(player_choice):
-    computer_choice = random.choice(["rock", "paper", "scissors"])
+    computer_choice = random.choice(["rock", "paper", "scissors","spock","lizard"])
     if player_choice == computer_choice:
         result = "It's a tie!"
     elif player_choice == "rock" and computer_choice == "scissors":
         result = "You win!"
+    elif player_choice == "rock" and computer_choice == "lizard":
+        result = "You win!"
     elif player_choice == "paper" and computer_choice == "rock":
+        result = "You win!"
+    elif player_choice == "paper" and computer_choice == "spock":
         result = "You win!"
     elif player_choice == "scissors" and computer_choice == "paper":
         result = "You win!"
+    elif player_choice == "scissors" and computer_choice == "lizard":
+        result = "You win!"
+    elif player_choice == "spock" and computer_choice == "rock":
+        result = "you win!"
+    elif player_choice == "spock" and computer_choice == "scissors":
+        result = "you win!"
+    elif player_choice == "lizard" and computer_choice == "spock":
+        result = "you win!"
+    elif player_choice == "lizard" and computer_choice == "paper":
+        result = "you win!"
     else:
         result = "You lose!"
     return result, computer_choice
